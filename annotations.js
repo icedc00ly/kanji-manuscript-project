5e9d092671a163679729e91bbb5bc507cb407fec
icedc00ly <124198488+icedc00ly@users.noreply.github.com>
2025-06-30 13:27:08 -0400

    Create annotations.js

---
+++ annotations.js
@@ -0,0 +1,32 @@
+const imageAnnotations = [
+  {
+    fileName: "kuruwa_no_tsuki.jpg",
+    title: "Moon of the Pleasure Quarters",
+    artist: "Tsukioka Yoshitoshi",
+    year: 1886,
+    kanji: [
+      {
+        character: "月",
+        reading: "つき",
+        meaning: "moon",
+        position: {
+          top: 16,
+          left: 60,
+          width: 5,
+          height: 7
+        }
+      },
+      {
+        character: "廓",
+        reading: "くるわ",
+        meaning: "pleasure quarters",
+        position: {
+          top: 16,
+          left: 45,
+          width: 5,
+          height: 7
+        }
+      }
+    ]
+  }
+];
