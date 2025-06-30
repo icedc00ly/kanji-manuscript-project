7e5e0bffc9b37e0d68028165b57b42e203f51c3f
icedc00ly <124198488+icedc00ly@users.noreply.github.com>
2025-06-30 14:15:18 -0400

    Update annotations.js

--- annotations.js
+++ annotations.js
@@ -11,7 +11,7 @@ const imageAnnotations = [
         meaning: "Night Scene / Moon of the Pleasure Quarters",
         position: {
           top: 45,
-          left: 435,
+          left: 310,
           width: 120,
           height: 180
         }
